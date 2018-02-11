9d5286ad79cd20829a04df12997f4856eac3519c
Andri Sudarmawijaya <andri.sudarmawijaya@gmail.com>
2018-02-11 21:37:55 +0700

    replare text
     On branch master
     Changes to be committed:
    	modified:   src/public/json/bantenprov/budget-absorption/budget-absorption-010.json
    	modified:   src/public/json/bantenprov/budget-absorption/budget-absorption-020.json
    	modified:   src/public/json/bantenprov/budget-absorption/budget-absorption-030.json
    	modified:   src/public/json/bantenprov/budget-absorption/budget-absorption-pie-010.json
    	modified:   src/public/json/bantenprov/budget-absorption/budget-absorption-pie-020.json
    	modified:   src/public/json/bantenprov/budget-absorption/budget-absorption-pie-030.json
    	modified:   src/resources/assets/js/components/bantenprov/budget-absorption/BudgetAbsorption.edit.vue
    	modified:   src/resources/assets/js/components/bantenprov/budget-absorption/BudgetAbsorption.index.vue
    	modified:   src/resources/assets/js/components/views/bantenprov/budget-absorption/BudgetAbsorptionBar02.vue
    	modified:   src/resources/assets/js/components/views/bantenprov/budget-absorption/BudgetAbsorptionBar03.vue
    	modified:   src/resources/assets/js/components/views/bantenprov/budget-absorption/BudgetAbsorptionPie02.vue
    	modified:   src/resources/assets/js/components/views/bantenprov/budget-absorption/BudgetAbsorptionPie03.vue
    	modified:   src/resources/assets/js/components/views/bantenprov/budget-absorption/DashboardBudgetAbsorption.vue

--- src/resources/assets/js/components/views/bantenprov/budget-absorption/DashboardBudgetAbsorption.vue
+++ src/resources/assets/js/components/views/bantenprov/budget-absorption/DashboardBudgetAbsorption.vue
@@ -25,7 +25,7 @@
         <div class="card h-100" style="color: #fff; background-color: #009688;">
           <div class="card-header">
             <i class="fa fa-bar-chart" aria-hidden="true"></i>
-            Rasio Ketersediaan Sekolah SMP-MTs Berdasarkan Wilayah
+            Anggaran dan Penyereapan Berdasarkan OPD
           </div>
           <div class="card-body">
             <budget-absorption-bar-01></budget-absorption-bar-01>
@@ -36,7 +36,7 @@
         <div class="card h-100" style="color: #fff; background-color: #FF9800;">
           <div class="card-header">
             <i class="fa fa-pie-chart" aria-hidden="true"></i>
-            Rasio Ketersediaan Sekolah SMP-MTs Berdasarkan Wilayah
+            Anggaran dan Penyereapan Berdasarkan OPD
           </div>
           <div class="card-body">
             <budget-absorption-pie-01></budget-absorption-pie-01>
@@ -52,7 +52,7 @@
         <div class="card h-100" style="color: #fff; background-color: #F44336;">
           <div class="card-header">
             <i class="fa fa-pie-chart" aria-hidden="true"></i>
-            Rasio Ketersediaan Sekolah SMP-MTs Berdasarkan Kategori
+            Anggaran dan penyerapan per bulan tiap OPD
           </div>
           <div class="card-body">
             <budget-absorption-pie-02></budget-absorption-pie-02>
@@ -63,7 +63,7 @@
         <div class="card text-white bg-dark h-100">
           <div class="card-header">
             <i class="fa fa-bar-chart" aria-hidden="true"></i>
-            Rasio Ketersediaan Sekolah SMP-MTs Berdasarkan Kategori
+            Anggaran dan penyerapan per bulan tiap OPD
           </div>
           <div class="card-body">
             <budget-absorption-bar-02></budget-absorption-bar-02>
